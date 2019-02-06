2a1a2985d8b4430eaad0569668528a330a6fc45f
Dennis Appelon Nielsen <da@aperta.dk>
2019-02-06 16:54:28 +0100

    Master commit

---
+++ sunset.py
@@ -0,0 +1,31 @@
+import urllib.request
+import datetime
+import json
+
+url = 'https://api.sunrise-sunset.org/json?lat=36.7201600&lng=4.4203400&formatted=0&date=today'
+
+req = urllib.request.Request(url)
+
+r = urllib.request.urlopen(req).read()
+
+data = json.loads(r)
+
+k = data["results"]
+
+sunset = k["sunset"]
+
+sunset_time_object = datetime.datetime.fromisoformat(sunset)
+
+print('Solen går ned idag kl:', sunset_time_object.time())
+
+timeis = datetime.datetime.now().strftime("%H:%M:%S")
+timeisobject = datetime.datetime.strptime(timeis, "%H:%M:%S")
+
+print('Klokken er nu: ', timeisobject.time())
+
+timeisobject = timeisobject.replace(tzinfo=None)
+sunset_time_object = sunset_time_object.replace(tzinfo=None)
+
+countdown = sunset_time_object - timeisobject
+
+print('Sekunder tilbage før solnedgang :', countdown.seconds)
